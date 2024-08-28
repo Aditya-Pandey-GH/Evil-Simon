e933d1d0838dc3aa01f7d8343eebba58a6b94ba7
Aditya Pandey <adityapandey1609@gmail.com>
2024-08-28 12:34:51 +0530

    v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainMenu from "./Comps/MainMenu";
 import Game from "./Comps/Game";
 import './Style.css';
 import GameOver from "./Comps/GameOver";
-import { useState } from "react";
 
 const App = () => {
 
 	return (
 		<>
-			<HashRouter basename={import.meta.env.BASE_URL}>
+			<BrowserRouter basename={import.meta.env.BASE_URL}>
 				<Routes>
 					<Route path="/" element={<MainMenu />} />
 					<Route path="/game" element={<Game />} />
 					<Route path="/gameOver" element={<GameOver />} />
 				</Routes>
-			</HashRouter>
+			</BrowserRouter>
 		</>
 	);
 };
